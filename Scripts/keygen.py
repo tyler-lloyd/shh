d7835d2807eafee91d91e715b48df2dc199d92b3
Tyler Lloyd <tylerlloyd@outlook.com>
2019-12-30 11:08:07 -0500

    Python script for generating AES keys

--- Scripts/keygen.py
+++ Scripts/keygen.py
@@ -1,7 +1,15 @@
 import os
+import base64
 
-def generate_aes_keys():
-    key = os.urandom()
+def generate_aes_keys() -> (str, str):
+    key = os.urandom(32) # 256 bit AES key
+    iv = os.urandom(16)
+    encoded_key = base64.b64encode(key)
+    encoded_iv = base64.b64encode(iv)
+    base64_key = str(encoded_key, 'utf-8')
+    base64_iv = str(encoded_iv, 'utf-8')
+    return (base64_key, base64_iv)
 
 if __name__ == "__main__":
-    pass
+    keys = generate_aes_keys()
+    print(f'AES 256 bit key: {keys[0]}\nAES IV: {keys[1]}')
